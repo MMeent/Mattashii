ef66cfb43e83a04385d2ce242778c901d3436385
mmeent <boekewurm@gmail.com>
2013-10-16 15:35:31 +0200

    added serializing functions. and stuff.

--- GBodies.py
+++ GBodies.py
@@ -2,19 +2,19 @@
 
 import math
 
-class SpatialData():
+class SpatialData(object):
     def __init__(self, x, y, z, vx, vy, vz):
-        self.body.sd._x, self.body.sd._y, self.body.sd._z = x, y, z
-        self.body.sd._vx, self.body.sd._vy, self.body.sd._vz = vx, vy, vz
+        self._x, self._y, self._z = x, y, z
+        self._vx, self._vy, self._vz = vx, vy, vz
 
     def __repr__(self):
         return "x:{x} y:{y} z:{z} vx:{vx} vy:{vy} vz:{vz}".format(
-            x  = self.body.sd._x,
-            y  = self.body.sd._y,
-            z  = self.body.sd._z,
-            vx = self.body.sd._vx,
-            vy = self.body.sd._vy,
-            vz = self.body.sd._vz)
+            x  = self._x,
+            y  = self._y,
+            z  = self._z,
+            vx = self._vx,
+            vy = self._vy,
+            vz = self._vz)
 
 
 #TODO: Time control
@@ -32,7 +32,7 @@ class Derivative(object):
     def __init__(self, dx, dy, dz, dvx, dvy, dvz):
         self._dx, self._dy, self._dz = dx, dy, dz
         self._dvx, self._dvy, self._dvz = dvx, dvy, dvz
-        self.body.ax, self.body.ay, self.body.az = 0., 0., 0.
+        self.ax, self.ay, self.az = 0., 0., 0.
 
     def __repr__(self):
         return "dx:{dx} dy:{dy} dz:{dz} dvx:{dvx} dvy:{dvy} dvz:{dvz}".format(
@@ -45,20 +45,31 @@ class Derivative(object):
 
 class Body(SpatialData):
     def __init__(self, x, y, z, vx, vy, vz, mass):
-        super(SpatialData, self).__init__(x, y, z, vx, vy, vz)
-        self.body.mass = mass
-        self.body.ax, self.body.ay, self.body.az = 0., 0., 0.
+        super(Body, self).__init__(x, y, z, vx, vy, vz)
+        self.mass = mass
+        self.ax, self.ay, self.az = 0., 0., 0.
 
     def __repr__(self):
         return " mass: {mass} \n {data}".format(
-            mass = self.body.mass,
-            data = repr(self.body.sd))
+            mass = self.mass,
+            data = super(Body, self).__repr__())
+
+    def serialize(self):
+        return { "body" : {
+                 "SpatialData" : { 
+                     "x" : self._x,
+                     "y" : self._y,
+                     "z" : self._z,
+                     "vx" : self._vx,
+                     "vy" : self._vy,
+                     "vz" : self._vz },
+                 "mass" : self.mass } }
 #todo: implement Time Control
     
     def distance(self, body):
-        dx = self.body.st._x - body.body.st._x
-        dy = self.body.st._y - body.body.st._y
-        dz = self.body.st._z - body.body.st._z
+        dx = self._x - body._x
+        dy = self._y - body._y
+        dz = self._z - body._z
         delta = math.sqrt(dx*dx + dy*dy + dz*dz)
         return delta
     
@@ -68,83 +79,116 @@ class Body(SpatialData):
     
     def CalcForce(self, body, Gc):
         delta = distance(body)
-        force = Gc * body.mass * self.body.mass / (delta*delta)
+        force = Gc * body.mass * self.mass / (delta*delta)
         return force
 
     def accelerate(self, body, Gc):
         Acc = CalcForce(body, Gc)
-        self.body._ax += force*delta(self.body.st._x, body.body.st._x)/distance(body)
-        self.body._ay += force*delta(self.body.st._y, body.body.st._y)/distance(body)
-        self.body._az += force*delta(self.body.st._z, body.body.st._z)/distance(body)
+        self._ax += force*delta(self._x, body._x)/distance(body)
+        self._ay += force*delta(self._y, body._y)/distance(body)
+        self._az += force*delta(self._z, body._z)/distance(body)
 
     def update(self, dt):
-        self.body.sd._x += self.body.sd._vx*dt
-        self.body.sd._y += self.body.sd._vy*dt
-        self.body.sd._z += self.body.sd._vz*dt
-        self.body.sd._vx += self.body._ax*dt
-        self.body.sd._vy += self.body._ay*dt
-        self.body.sd._vz += self.body._az*dt
-        self.body._ax, self.body._ay, self.body._az = 0., 0., 0.
+        self._x += self._vx*dt
+        self._y += self._vy*dt
+        self._z += self._vz*dt
+        self._vx += self._ax*dt
+        self._vy += self._ay*dt
+        self._vz += self._az*dt
+        self._ax, self._ay, self._az = 0., 0., 0.
 
-class Sattelite(Body):
+class Satellite(Body):
     def __init__(self, x, y, z, vx, vy, vz, mass, name):
-        super(Body, self).__init__(x, y, z, vx, vy, vz, mass)
+        super(Satellite, self).__init__(x, y, z, vx, vy, vz, mass)
         self._name = name
 
     def __repr__(self):
         return " Sattelite: {name} \n{data}".format(
             name = self._name,
-            data = super(Body, self).__repr__())
+            data = super(Satellite, self).__repr__())
+
+    def serialize(self):
+        data = super(Satellite, self).serialize()
+        data.update({ "name" : self._name })
+        return data
 
 class Moon(Body):
     def __init__(self, x, y, z, vx, vy, vz, mass, radius, aPressure, name):
-        super(Body, self).__init__(x, y, z, vx, vy, vz, mass)
-        self.body._rad = radius
+        super(Moon, self).__init__(x, y, z, vx, vy, vz, mass)
+        self._rad = radius
         self._pre = aPressure
         self._name = name
 
     def __repr__(self):
         return " Moon: {name} \n radius: {rad} \n pressure: {pre} \n{data}".format(
             name = self._name,
-            rad = self.body._rad,
+            rad = self._rad,
             pre = self._pre,
-            data = super(Body, self).__repr__())
+            data = super(Moon, self).__repr__())
+
+    def serialize(self):
+        data = super(Moon, self).serialize()
+        data.update({ "body" : { "radius" : self._rad },
+                      "name" : self._name,
+                      "pressure" : self._pre,
+                    }
 
 class Planet(Body):
     def __init__(self, x, y, z, vx, vy, vz, mass, radius, aPressure, name):
-        super(Body, self).__init__(x, y, z, vx, vy, vz, mass)
-        self.body._rad = radius
+        super(Planet, self).__init__(x, y, z, vx, vy, vz, mass)
+        self._rad = radius
         self._pre = aPressure
         self._name = name
 
     def __repr__(self):
         return " Planet: {name} \n radius: {rad} \n pressure: {pre} \n{data}".format(
             name = self._name,
-            rad = self.body._rad,
+            rad = self._rad,
             pre = self._pre,
-            data = super(Body, self).__repr__())
+            data = super(Planet, self).__repr__())
+
+
+    def serialize(self):
+        data = super(Planet, self).serialize()
+        data.update({ "body" : { "radius" : self._rad },
+                      "name" : self._name,
+                      "pressure" : self._pre,
+                    }
 
 
 class Star(Body):
     def __init__(self, x, y, z, vx, vy, vz, mass, radius, name):
-        super(Body, self).__init__(x, y, z, vx, vy, vz, mass)
-        self.body._rad = radius
+        super(Star, self).__init__(x, y, z, vx, vy, vz, mass)
+        self._rad = radius
         self._name = name
 
     def __repr__(self):
         return " Star: {name} \n radius: {rad} \n{data}".format(
             name = self._name,
-            rad = self.body._rad,
-            data = super(Body, self).__repr__())
+            rad = self._rad,
+            data = super(Star, self).__repr__())
+
+    def serialize(self):
+        data = super(Star, self).serialize()
+        data.update({ "body" : { "radius" : self._rad },
+                      "name" : self._name,
+                    }
+
 
 class Astroid(Body):
     def __init__(self, x, y, z, vx, vy, vz, mass, radius, name):
-        super(Body, self).__init__(x, y, z, vx, vy, vz, mass)
-        self.body._rad = radius
+        super(Star, self).__init__(x, y, z, vx, vy, vz, mass)
+        self._rad = radius
         self._name = name
 
     def __repr__(self):
         return " Astroid: {name} \n radius: {rad} \n{data}".format(
             name = self._name,
-            rad = self.body._rad,
-            data = super(Body, self).__repr__())
+            rad = self._rad,
+            data = super(Star, self).__repr__())
+    def serialize(self):
+        data = super(Moon, self).serialize()
+        data.update({ "body" : { "radius" : self._rad },
+                      "name" : self._name,
+                    }
+
--- Sim.py
+++ Sim.py
@@ -14,18 +14,91 @@ def main():
     GravityConstant = data["GravityConstant"]
     print "Gravitational Constant: {gc}".format(gc = GravityConstant)
     
+    stars = []
     jsonStars = data["Stars"]
-    jsonPlanets = data["Planets"]
-    jsonMoons = data["Moons"]
-    jsonAstroids = data["Astroids"]
-    jsonSatellites = data["Satellites"]
     print "Stars: {data}".format(data = jsonStars)
+    for star in jsonStars:
+        bdata = star["body"]
+        spdata = bdata["SpatialData"]
+        stars.append(GBodies.Star(
+            spdata["x"], 
+            spdata["y"], 
+            spdata["z"], 
+            spdata["vx"],
+            spdata["vy"],
+            spdata["vz"],
+            bdata["mass"],
+            bdata["radius"],
+            star["name"]))
+    planets = []
+    jsonPlanets = data["Planets"]
     print "Planets: {data}".format(data = jsonPlanets)
+    for planet in jsonPlanets:
+        bdata = planet["body"]
+        spdata = bdata["SpatialData"]
+        planets.append(GBodies.Planet(
+            spdata["x"],
+            spdata["y"],
+            spdata["z"],
+            spdata["vx"],
+            spdata["vy"],
+            spdata["vz"],
+            bdata["mass"],
+            bdata["radius"],
+            planet["SurfacePressure"],
+            planet["name"]))
+
+    moons = []
+    jsonMoons = data["Moons"]
     print "Moons: {data}".format(data = jsonMoons)
+    for moon in jsonMoons:
+        bdata = moon["body"]
+        spdata = bdata["SpatialData"]
+        moons.append(GBodies.Moon(
+            spdata["x"],
+            spdata["y"],
+            spdata["z"],
+            spdata["vx"],
+            spdata["vy"],
+            spdata["vz"],
+            bdata["mass"],
+            bdata["radius"],
+            moon["SurfacePressure"],
+            moon["name"]))
+
+    astroids = []
+    jsonAstroids = data["Astroids"]
     print "Astroids: {data}".format(data = jsonAstroids)
+    for astroid in jsonAstroids:
+        bdata = astroid["body"]
+        spdata = bdata["SpatialData"]
+        astroids.append(GBodies.Astroid(
+            spdata["x"],
+            spdata["y"],
+            spdata["z"],
+            spdata["vx"],
+            spdata["vy"],
+            spdata["vz"],
+            bdata["mass"],
+            bdata["radius"],
+            astroid["name"]))
+
+    satellites = []
+    jsonSatellites = data["Satellites"]
     print "Satellites: {data}".format(data = jsonSatellites)
-    
-    
+    for satellite in jsonSatellites:
+        bdata = satellite["body"]
+        spdata = satellite["SpatialData"]
+        satellite.append(GBodies.Satellite(
+            spdata["x"],
+            spdata["y"],
+            spdata["z"],
+            spdata["vx"],
+            spdata["vy"],
+            spdata["vz"],
+            bdata["mass"],
+            satellite["name"]))
+
 
 if __name__ == '__main__':
     main()
