f10e6e1f471ec226a52a7e879fa93ce8c35cb4a0
mmeent <boekewurm@gmail.com>
2013-11-29 11:01:39 +0100

    Did work on the UI, now input works, only the reading has to be worked on some more. Little bugged right now. Made good progress.

--- mattashii_ui/controllers.py
+++ mattashii_ui/controllers.py
@@ -1,5 +1,7 @@
 __author__ = 'matthias'
 
+from gi.repository import Gtk
+
 import mattashii
 
 import view as view
@@ -19,12 +21,35 @@ class Update(object):
 
 class newSimulation(object):
     @staticmethod
-    def create(Window):
-        with open(mattashii.main(None, None, None, None)) as data:
-            NewWindow= newSimulation.Plot(data, Window)
+    def create(Box):
+        Items = Box.get_children()
+        for i in Items:
+            for j in i:
+                name = Gtk.Buildable.get_name(j)
+                print name
+                print type(j)
+                if name == "FileChosen":
+                    InFile = j.get_filename()
+                elif name == "OutFile":
+                    OutFileName = j.get_text()
+                elif name == "PrecisionSlide":
+                    precision = j.get_value()
+                elif name == "TimePlot":
+                    TimePlot = j.get_value()
+                elif name == "StepsSlide":
+                    Steps = int(j.get_value())
+        if OutFileName == None:
+            OutFileName = "Bodies"
+        if InFile == None:
+            InFile == "Bodies.json"
+        print InFile, OutFileName, precision, TimePlot, Steps
+        WriteDTime = TimePlot / Steps
+        for i in range(Steps):
+            InFileName = InFile + i + ".json"
+            with open(mattashii.main(InFileName, OutFileName, precision, WriteDTime)) as data:
+                NewPlot = newSimulation.Plot(data, plot)
 
     @staticmethod
     def Plot(data, plot):
-        pass
         NewWindow = Update.Plot(plot)
         return NewWindow
